bdd412118cd4980994b52e1971379f517c46e68b
Henley Huang <henthebomb3@gmail.com>
2022-12-06 19:56:40 -0600

    Added foo to the bar

---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const projects = [
+  {
+    title: "My Portfolio",
+    subtitle: "Project Link",
+    description:
+      "This is my first project! I built this with Javascript, html, css and ReactJS!",
+    image: "./project-1.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+
+export const skills = [
+  "Java",
+  "Python",
+  "C++",
+  "HTML/CSS",
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "fucking my girl goooodd",
+];
